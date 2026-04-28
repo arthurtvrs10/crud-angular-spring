8371d7af0907aa7f812e9fb8c56280ef897d2b30
tavares <arthurtavares0222@gmail.com>
2026-04-28 15:27:01 -0300

    feat: cria modulo de cursos e usa renderização com lazy loading

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'courses', // Redirect to the 'courses' path when the root path is accessed
+    pathMatch: 'full', // Ensure that the entire URL matches the empty path for redirection
+  },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses-module').then(mod => mod.CoursesModule),
+  }
+];
