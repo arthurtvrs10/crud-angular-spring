738402e9f6dac8f42912792a8aa402ec59671014
tavares <arthurtavares0222@gmail.com>
2026-04-28 15:01:09 -0300

    feat: adiciona tool-bar

--- crud-angular/src/app/app.ts
+++ crud-angular/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule }  from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
